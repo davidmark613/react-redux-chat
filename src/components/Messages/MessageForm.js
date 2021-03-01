3a4e4a0e2143e5d4c53a1e0f206bac0417338bfa
David Mark <dovidmark@icloud.com>
2021-03-02 00:58:51 +0200

    add functionality to change and edit user avatar images

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -8,6 +8,7 @@ import ProgressBar from "./ProgressBar";
 class MessageForm extends Component {
     state = {
         storageRef: firebase.storage().ref(),
+        typingRef: firebase.database().ref('typing'),
         uploadTask: null,
         uploadState: '',
         percentUploaded: 0,
@@ -25,7 +26,23 @@ class MessageForm extends Component {
 
     handleChange = event => {
         this.setState({ [event.target.name]: event.target.value });
-    }
+    };
+
+    handleKeyDown = () => {
+        const {message, typingRef, channel, user} = this.state;
+
+        if (message) {
+            typingRef
+                .child(channel.id)
+                .child(user.uid)
+                .set(user.displayName)
+        } else {
+            typingRef
+                .child(channel.id)
+                .child(user.uid)
+                .remove();
+        }
+    };
 
     createMessage = (fileUrl = null) => {
         const message = {
@@ -46,7 +63,7 @@ class MessageForm extends Component {
 
     sendMessage = () => {
         const {getMessagesRef} = this.props;
-        const {message, channel} = this.state;
+        const {message, channel, user, typingRef} = this.state;
 
         if (message) {
             this.setState({loading: true});
@@ -55,7 +72,11 @@ class MessageForm extends Component {
                 .push()
                 .set(this.createMessage())
                 .then(() => {
-                    this.setState({loading: false, message: '', errors: []})
+                    this.setState({loading: false, message: '', errors: []});
+                    typingRef
+                        .child(channel.id)
+                        .child(user.uid)
+                        .remove();
                 })
                 .catch(error => {
                     console.error(error);
@@ -148,6 +169,7 @@ class MessageForm extends Component {
                     fluid
                     name='message'
                     onChange={this.handleChange}
+                    onKeyDown={this.handleKeyDown}
                     value={message}
                     style={{marginBottom: '0.7em'}}
                     label={<Button icon={'add'}/>}
